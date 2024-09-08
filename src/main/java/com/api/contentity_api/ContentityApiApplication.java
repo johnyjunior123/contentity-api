5d01bc793e701bb7ced27dc07dbc1f830cffefa3
Johny Junior <johnyjunior64@gmail.com>
2024-09-08 20:53:18 -0300

    initial commit

---
+++ src/main/java/com/api/contentity_api/ContentityApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.contentity_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentityApiApplication.class, args);
+	}
+
+}
